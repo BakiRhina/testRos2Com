30dd95655184cd478f3f375cf47bbe8569c6cd06
8kyu <guillemsenabre@gmail.com>
2023-10-19 16:18:33 +0800

    added comments to publisher.py

--- src/pyPubsub/pyPubsub/publisher_member_function.py
+++ src/pyPubsub/pyPubsub/publisher_member_function.py
@@ -1,29 +1,32 @@
-# Copyright 2016 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import rclpy # ROS Client library for python
 
-import rclpy
-from rclpy.node import Node
+from rclpy.node import Node # basic class entity in ROS to subscribe and publish to nodes
+from std_msgs.msg import String # message type from the 'std_msgs' package representing a simple string
 
-from std_msgs.msg import String
-
+# By passing Node class as an argument of MinimalPublisher class, we are saying that
+#this is also a Node, and all properties and methods are inherited by this.
 
 class MinimalPublisher(Node):
 
+    # This is the constructor of the class that will be automatically called when
+    #this object (class) is instantiated. In py the constructor is called __init__
+
     def __init__(self):
+
+        # - This is calling the constructor (__init__) of the parent class (Node) and it's
+        #basically creating a 'Node' with the name 'minimal_publisher'
         super().__init__('minimal_publisher')
+
+        # - create_publisher is a method from 'Node' class (from 'rclpy.node') that creates
+        #a publisher object that can send (publish) messages to a topic. In this case
+        #is publishing a message of type 'String' on a topic named 'topic'.
+        # - The integer '10' is a required QoS (Quality of Service) setting that limits the
+        #amount of queued messages if a subscriber is not receiving them fast enough.
         self.publisher_ = self.create_publisher(String, 'topic', 10)
         timer_period = 0.5  # seconds
+
+        # - create_timer is a method from the Node class that sets up a timer to call
+        #a specific function periodically (in this case the function timer_callback)
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
